88c6a53151f83284020229e5211c2cf99ce17bbb
ceasar13m <muharlyamovy@gmail.com>
2019-07-19 18:10:00 +0300

    Добавление, удаление, получение списка всех учителей, получение списка всех студентов и студентов по gradeId Учтены варианты неправильного ввода полей для получения списка студентов Исправлена ошибка с некорректными полями json Работает назначение учителю предметов\
    JETTY integration
    
    frontend
    
    Менюшка, СиЭсЭс, фся вигня
    
    Отображаются таблицы

--- src/main/webapp/WEB-INF/scriptTeachers.js
+++ src/main/webapp/WEB-INF/scriptTeachers.js
@@ -1,9 +1,9 @@
 document.getElementById("fountainG").style.visibility = 'hidden';
 let teachersButton = document.getElementById("teachersButton");
+let teacherStorage = [];
 
 
-
-teachersButton.onclick = function() {
+teachersButton.onclick = function () {
     document.getElementById("content").innerHTML = "";
     document.getElementById("fountainG").style.visibility = 'visible';
 
@@ -26,14 +26,23 @@ teachersButton.onclick = function() {
 
             content.appendChild(trT);
 
+
             for (let i = 0; i < teachers.length; i++) {
+                var teacherObject = {
+                    column1Value: teachers[i].firstName,
+                    column1Ref: document.createElement('td'),
+                    column2Value: teachers[i].secondName,
+                    column2Ref: document.createElement('td')
+                };
+
+                teacherStorage.push(teacherObject);
                 let tr = document.createElement("tr");
 
-                let td1 = document.createElement("td");
-                let td2 = document.createElement("td");
+                let td1 = teacherObject.column1Ref;
+                let td2 = teacherObject.column2Ref;
 
-                td1.innerHTML = teachers[i].firstName;
-                td2.innerHTML = teachers[i].secondName;
+                td1.innerHTML = teacherObject.column1Value;
+                td2.innerHTML = teacherObject.column2Value;
 
 
                 tr.appendChild(td1);
@@ -50,3 +59,81 @@ teachersButton.onclick = function() {
 
 
 
+
+
+var table = document.getElementById('content');
+
+var editingTd;
+
+table.onclick = function(event) {
+
+    var target = event.target;
+
+    while (target != table) {
+        if (target.className == 'edit-cancel') {
+            finishTdEdit(editingTd.elem, false);
+            return;
+        }
+
+        if (target.className == 'edit-ok') {
+            finishTdEdit(editingTd.elem, true);
+            return;
+        }
+
+        if (target.nodeName == 'TD') {
+            if (editingTd) return; // already editing
+
+            makeTdEditable(target);
+            return;
+        }
+
+        target = target.parentNode;
+    }
+}
+
+function makeTdEditable(td) {
+    editingTd = {
+        elem: td,
+        data: td.innerHTML
+    };
+
+    td.classList.add('edit-td'); // td, not textarea! the rest of rules will cascade
+
+    var textArea = document.createElement('textarea');
+    textArea.style.width = td.clientWidth + 'px';
+    textArea.style.height = td.clientHeight + 'px';
+    textArea.className = 'edit-area';
+
+    textArea.value = td.innerHTML;
+    td.innerHTML = '';
+    td.appendChild(textArea);
+    textArea.focus();
+
+    td.insertAdjacentHTML("beforeEnd",
+        '<div class="edit-controls"><button class="edit-ok">OK</button><button class="edit-cancel">CANCEL</button></div>'
+    );
+}
+
+function finishTdEdit(td, isOk) {
+    if (isOk) {
+        td.innerHTML = td.firstChild.value;
+
+        xyu(td);
+    } else {
+        td.innerHTML = editingTd.data;
+    }
+    td.classList.remove('edit-td'); // remove edit class
+    editingTd = null;
+}
+
+
+function xyu(td) {
+    let x;
+    for(let i = 0; i < teacherStorage.length; i ++) {
+        if(teacherStorage[i].column2Ref == td) {
+            x = td.id;
+            console.log(x);
+        }
+    }
+
+}
